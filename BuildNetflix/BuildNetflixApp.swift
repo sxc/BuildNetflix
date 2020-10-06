99da2a068d6c5ff7bc9d0957635b372f405ba56e
sxc <qiuyequ@icloud.com>
2020-10-06 08:20:25 +0800

    Initial Commit

---
+++ BuildNetflix/BuildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixApp.swift
+//  BuildNetflix
+//
+//  Created by Xiaochun Shen on 2020/10/6.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
