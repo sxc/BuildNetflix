52d86f9ea3f7765c5e82520464a6ba6463435bbe
sxc <qiuyequ@icloud.com>
2020-10-10 15:04:40 +0800

    add cast information

--- BuildNetflix/Helpers/GlobalHelpers.swift
+++ BuildNetflix/Helpers/GlobalHelpers.swift
@@ -8,16 +8,69 @@
 import Foundation
 import SwiftUI
 
-let exampleMovie1 = Movie(id: UUID().uuidString, name: "DARK", thumbnailURL: URL(string: "https://picsum.photos/200/300")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 1, promostionHeadline: "Best Rated Show")
+let exampleMovie1 = Movie(
+    id: UUID().uuidString,
+    name: "Community",
+    thumbnailURL: URL(string: "https://picsum.photos/200/300")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020,
+    rating: "TV-MA",
+    numberOfSeasons: 2,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baran bo Odan, Jantje Friese",
+    cast: "Louis Hofmann, Oliver Masucci, jordis Triebel",
+    promostionHeadline: "Best Rated Show")
 
-let exampleMovie2 = Movie(id: UUID().uuidString, name: "Travelers", thumbnailURL: URL(string: "https://picsum.photos/200/300/")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 2, promostionHeadline: "Best Rated Show")
-let exampleMovie3 = Movie(id: UUID().uuidString, name: "Community", thumbnailURL: URL(string: "https://picsum.photos/200/301")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 3, promostionHeadline: "Best Rated Show")
-let exampleMovie4 = Movie(id: UUID().uuidString, name: "Alone", thumbnailURL: URL(string: "https://picsum.photos/200/302")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 4,  promostionHeadline: "Best Rated Show")
-let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 5,  promostionHeadline: "Best Rated Show")
-let exampleMovie6 = Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 6, promostionHeadline: "Best Rated Show")
+
+let exampleMovie2 = Movie(
+    id: UUID().uuidString,
+    name: "Community",
+    thumbnailURL: URL(string: "https://picsum.photos/200/300/")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020, rating: "TV-MA",
+    numberOfSeasons: 3,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baran bo Odan, Jantje Friese",
+    cast: "Louis Hofmann, Oliver Masucci, jordis Triebel",
+    promostionHeadline: "Best Rated Show")
+
+let exampleMovie3 = Movie(
+    id: UUID().uuidString,
+    name: "Community",
+    thumbnailURL: URL(string: "https://picsum.photos/200/301")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020, rating: "TV-MA",
+    numberOfSeasons: 3,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baran bo Odan, Jantje Friese",
+    cast: "Louis Hofmann, Oliver Masucci, jordis Triebel",
+    promostionHeadline: "Best Rated Show")
+
+let exampleMovie4 = Movie(
+    id: UUID().uuidString,
+                          
+    name: "Alone",
+    thumbnailURL: URL(string: "https://picsum.photos/200/302")!,
+    categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"],
+    year: 2020, rating: "TV-MA",
+    numberOfSeasons: 4,
+    defaultEpisodeInfo: exampleEpisodeInfo1,
+    creators: "Baran bo Odan, Jantje Friese",
+                          cast: "Louis Hofmann, Oliver Masucci, jordis Triebel", promostionHeadline: "Best Rated Show")
+
+let exampleMovie5 = Movie(id: UUID().uuidString, name: "Hannibal", thumbnailURL: URL(string: "https://picsum.photos/200/303")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 5,  defaultEpisodeInfo: exampleEpisodeInfo1,creators: "Baran bo Odan, Jantje Friese",
+                          cast: "Louis Hofmann, Oliver Masucci, jordis Triebel", promostionHeadline: "Best Rated Show")
+
+let exampleMovie6 = Movie(id: UUID().uuidString, name: "After Life", thumbnailURL: URL(string: "https://picsum.photos/200/304")!, categories: ["Dystopian", "Exciting", "Suspenseful", "Sci-Fi TV"], year: 2020, rating: "TV-MA", numberOfSeasons: 6, defaultEpisodeInfo: exampleEpisodeInfo1, creators: "Baran bo Odan, Jantje Friese",
+                          cast: "Louis Hofmann, Oliver Masucci, jordis Triebel",promostionHeadline: "Best Rated Show")
 
 
 let exampleMovies: [Movie] = [exampleMovie1,exampleMovie2,exampleMovie3,exampleMovie4,exampleMovie5,exampleMovie6]
+
+let exampleEpisodeInfo1 = CurrentEpisodeInfo(episodeName: "Beginnings and Endings",
+                                             description: "Six months after the disappearances, the police form a task force. In 2052, Jonas learns that most of Winden perished in an apocalyptic event.", season: 1,
+                                             episode: 1)
+
 
 extension LinearGradient {
     static let blackOpacityGradient = LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.0), Color.black.opacity(0.95)]),
--- BuildNetflix/MovieDetail.swift
+++ BuildNetflix/MovieDetail.swift
@@ -50,6 +50,11 @@ struct MovieDetail: View {
                             //
                         }
                         
+                        // Current Episode Information
+                        // 1. Default
+                        // 2. Personalized - where the user currently is
+                        CurrentEpisodeInformation(movie: movie)
+                        CastInfo(movie: movie)
                        
 
                 }
@@ -102,5 +107,48 @@ struct RatingView: View {
                 .bold()
         }
         .frame(width: 50, height: 20)
+    }
+}
+
+struct CastInfo: View {
+    var movie: Movie
+    var body: some View {
+        VStack(spacing: 3) {
+            HStack {
+                Text("Cast: \(movie.cast)")
+                
+                Spacer()
+            }
+            
+            HStack {
+                Text("Creator: \(movie.creators)")
+                
+                Spacer()
+            }
+        }
+        .font(.caption)
+        .foregroundColor(.gray)
+        .padding(.vertical, 10)
+    }
+}
+
+struct CurrentEpisodeInformation: View {
+    var movie: Movie
+    var body: some View {
+        Group {
+            
+            HStack {
+                Text(movie.episodeInfoDisplay)
+                    .bold()
+                Spacer()
+            }
+            .padding(.vertical, 4)
+            HStack {
+                Text(movie.episodeDescriptionDisplay)
+                    .font(.subheadline)
+                Spacer()
+            }
+            
+        }
     }
 }
